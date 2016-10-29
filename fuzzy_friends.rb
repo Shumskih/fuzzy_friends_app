4b7f4381337bc68ab706db1325e9af6a6cd3835a
Shumskih <music.dll@mail.ru>
2016-10-29 09:29:09 +0300

    Added classes Cat and Bird

--- fuzzy_friends.rb
+++ fuzzy_friends.rb
@@ -1,3 +1,15 @@
+class Bird
+    def talk
+        puts "Chik-chirik!"
+    end
+
+
+    def move(destination)
+        puts "Flying to the #{destination}"
+    end
+end
+
+
 class Dog
     def talk
       puts "Guv!"
@@ -6,5 +18,16 @@ class Dog
 
     def move(destination)
       puts "Running to the #{destination}."
+    end
+end
+
+class Cat
+    def talk
+        puts "Meow!"
+    end
+
+
+    def move
+        puts "Running to the #{destination}"
     end
 end
