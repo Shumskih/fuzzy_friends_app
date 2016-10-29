d8b9479ff4d88d225d0524549c08350dc7dfc039
Shumskih <music.dll@mail.ru>
2016-10-29 14:38:08 +0300

    Added code of class Dog

---
+++ dog.rb
@@ -0,0 +1,40 @@
+class Dog
+
+    attr_reader :name, :age
+
+    def name=(value)
+      if value == ""
+        raise "Name can't be blank!"
+      else
+        @name = value
+      end
+    end
+
+    def age=(value)
+        if value < 0
+            raise "An age of #{value} isn't valid!"
+        else
+            @age = value
+        end
+    end
+
+    def report_age
+        puts "#{@name} is #{@age} years old."
+    end
+
+    def talk
+      puts "#{@name} says Guv!"
+    end
+
+
+    def move(destination)
+      puts "#{@name} runs to the #{destination}."
+    end
+end
+
+dog = Dog.new
+dog.name = "Daisy"
+dog.age = 3
+dog.report_age
+dog.talk
+dog.move("bed")
